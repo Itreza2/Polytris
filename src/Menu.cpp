0b06da74ea8829a445d79a35be871895d0d74d4a
BOSSY <louis.bossy@etu.univ-tours.fr>
2025-10-18 18:48:29 +0200

    animated background

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -32,9 +32,28 @@ void Menu::render()
 {
 	SDL_Rect p1Area = { 192, 212, 576, 656 };
 	SDL_Rect p2Area = { 1152, 212, 576, 656 };
+	SDL_Rect dst;
+	int tick = (int)(SDL_GetTicks() * 0.15) % 761;
 
-	SDL_Rect src = { 16, 0, 16, 16 };
-	SDL_RenderCopy(Window::getWindow()->renderer, AssetsManager::getLib()->getTexture("blocks"), &src, NULL);
+	SDL_RenderCopy(Window::getWindow()->renderer, AssetsManager::getLib()->getTexture("background"), NULL, NULL);
+	for (int i = - 1600; i < 800; i += 766) {
+		dst = { 1600, - i - tick, 1920, 1600 };
+		SDL_RenderCopy(Window::getWindow()->renderer, AssetsManager::getLib()->getTexture("redBand"), NULL, &dst);
+		dst = { 0, - i - tick, 1920, 1600 };
+		SDL_RenderCopy(Window::getWindow()->renderer, AssetsManager::getLib()->getTexture("redBand"), NULL, &dst);
+		dst = { -1600, - i - tick, 1920, 1600 };
+		SDL_RenderCopy(Window::getWindow()->renderer, AssetsManager::getLib()->getTexture("redBand"), NULL, &dst);
+	}
+	for (int i = -1600; i < 800; i += 766) {
+		dst = { 1600, i + tick, 1920, 1600 };
+		SDL_RenderCopy(Window::getWindow()->renderer, AssetsManager::getLib()->getTexture("blueBand"), NULL, &dst);
+		dst = { 0, i + tick, 1920, 1600 };
+		SDL_RenderCopy(Window::getWindow()->renderer, AssetsManager::getLib()->getTexture("blueBand"), NULL, &dst);
+		dst = { -1600, i + tick, 1920, 1600 };
+		SDL_RenderCopy(Window::getWindow()->renderer, AssetsManager::getLib()->getTexture("blueBand"), NULL, &dst);
+	}
+	SDL_RenderCopy(Window::getWindow()->renderer, AssetsManager::getLib()->getTexture("alpha50"), NULL, NULL);
+
 
 	SDL_RenderCopy(Window::getWindow()->renderer, player1->render(), NULL, &p1Area);
 	SDL_RenderCopy(Window::getWindow()->renderer, player2->render(), NULL, &p2Area);
